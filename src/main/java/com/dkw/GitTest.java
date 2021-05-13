1ff121f6979fab20ec8d7a06023fa1b0dc29ba6f
Kerry <www.iedukewei@163.com>
2021-05-13 11:35:04 +0800

    master push2 commit

--- src/main/java/com/dkw/GitTest.java
+++ src/main/java/com/dkw/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println(" master git2");
         System.out.println(" hot-fix git2");
         System.out.println("push test!");
+        System.out.println("push test2!");
     }
 }
