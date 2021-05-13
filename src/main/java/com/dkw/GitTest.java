4fdcf6d9201446c55255de4f64b3f82b1a6623f6
Kerry <www.iedukewei@163.com>
2021-05-13 10:28:25 +0800

    master second commit

--- src/main/java/com/dkw/GitTest.java
+++ src/main/java/com/dkw/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("git");
         System.out.println(" hot-fix git");
+        System.out.println(" hot-fix git2");
     }
 }
